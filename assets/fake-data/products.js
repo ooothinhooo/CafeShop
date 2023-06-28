292b4b1df38329bf0cd8aa75df20be96d72f49fd
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-06-29 02:38:21 +0700

    update v4

---
+++ assets/fake-data/products.js
@@ -0,0 +1,691 @@
+
+
+const products = [
+  {
+    id: "01",
+    title: "Trà sữa truyền thống",
+    price: 22,
+    category: "TRÀ SỮA",
+  }, {
+    id: "02",
+    title: "Trà sữa MATCHA",
+    price: 25,
+    category: "TRÀ SỮA",
+  }, {
+    id: "03",
+    title: "Trà sữa khoai môn",
+    price: 25,
+    category: "TRÀ SỮA",
+  }, {
+    id: "04",
+    title: "Trà sữa thái xanh",
+    price: 25,
+    category: "TRÀ SỮA",
+  }, {
+    id: "05",
+    title: "Trà sữa thái đỏ",
+    price: 25,
+    category: "TRÀ SỮA",
+  },
+  {
+    id: "06",
+    title: "Sữa tươi chân châu đường đen",
+    price: 25,
+    category: "TRÀ SỮA",
+  },
+// 
+{
+  id: "suachua01",
+  title: "Sữa chua VIỆT quốc",
+  price: 25,
+  category: "SỮA CHUA",
+},
+{
+  id: "suachua02",
+  title: "Sữa chua đá",
+  price: 20,
+  category: "SỮA CHUA",
+},
+
+{
+  id: "suachua03",
+  title: "Sữa chua phúc bồ tử",
+  price: 25,
+  category: "SỮA CHUA",
+},
+{
+  id: "suachua04",
+  title: "Sữa chua dâu",
+  price: 25,
+  category: "SỮA CHUA",
+},
+{
+  id: "suachua05",
+  title: "Sữa chua kiwi",
+  price: 25,
+  category: "SỮA CHUA",
+},
+{
+  id: "suachua06",
+  title: "Sữa chua đào",
+  price: 25,
+  category: "SỮA CHUA",
+},
+// 
+{
+  id: "tra01",
+  title: "Trà dưa lưới",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà chanh",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà đào",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà hạt sen",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà đào cam sả",
+  price: 25,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà lipton đá",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà chanh dây",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà dâu",
+  price: 20,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà mãng cầu",
+  price: 25,
+  category: "TRÀ",
+},{
+  id: "tra01",
+  title: "Trà trái cây nhiệt đới",
+  price: 25,
+  category: "TRÀ",
+},
+// 
+{
+  id: "yAourt01",
+  title: "yAourt kiwi dầm ",
+  price: 30,
+  category: "YAOURT",
+},
+{
+  id: "yAourt02",
+  title: "yAourt dâu dầm",
+  price: 30,
+  category: "YAOURT",
+},{
+  id: "yAourt03",
+  title: "yAourt nho dầm",
+  price: 30,
+  category: "YAOURT",
+},{
+  id: "yAourt04",
+  title: "yAourt chanh dây dầm",
+  price: 25,
+  category: "YAOURT",
+},{
+  id: "yAourt05",
+  title: "yAourt bơ dầm",
+  price: 30,
+  category: "YAOURT",
+},
+
+// 
+{
+  id: "sinhto01",
+  title: "Sinh tố dâu",
+  price: 25,
+  category: "SINH TỐ",
+},
+{
+  id: "sinhto02",
+  title: "Sinh tố bơ ",
+  price: 25,
+  category: "SINH TỐ",
+},{
+  id: "sinhto03",
+  title: "Sinh tố sapo",
+  price: 25,
+  category: "SINH TỐ",
+},{
+  id: "sinhto04",
+  title: "Sinh tố kiwi",
+  price: 30,
+  category: "SINH TỐ",
+},{
+  id: "sinhto05",
+  title: "Sinh tố chanh tuyết",
+  price: 25,
+  category: "SINH TỐ",
+},{
+  id: "sinhto06",
+  title: "Sinh tố mãng cầu",
+  price: 25,
+  category: "SINH TỐ",
+},{
+  id: "sinhto07",
+  title: "Sinh tố dưa lưới",
+  price: 30,
+  category: "SINH TỐ",
+},{
+  id: "sinhto08",
+  title: "Sinh tố cam",
+  price: 25,
+  category: "SINH TỐ",
+},{
+  
+  id: "sinhto09",
+  title: "Sinh tố dừa",
+  price: 25,
+  category: "SINH TỐ",
+},
+
+
+//
+{
+  id: "daxay01",
+  title: "Đá Xay việt quất",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay02",
+  title: "Đá Xay matcha",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay03",
+  title: "Đá Xay kiwi",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay04",
+  title: "Đá Xay chanh dây",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay05",
+  title: "Đá Xay dâu",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay06",
+  title: "Đá Xay hoa hồng",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay07",
+  title: "Đá Xay chanh tuyết",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay08",
+  title: "Đá Xay biển xanh",
+  price: 25,
+  category: "ĐÁ XAY",
+},{
+  id: "daxay09",
+  title: "Đá Xay cookie original",
+  price: 25,
+  category: "ĐÁ XAY",
+},
+//
+{
+  id: "nuocep01",
+  title: "NƯớc ép khóm",
+  price: 25,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep02",
+  title: "NƯớc ép bưởi",
+  price: 25,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep03",
+  title: "NƯớc ép chanh dây",
+  price: 20,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep04",
+  title: "NƯớc ép táo",
+  price: 30,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep05",
+  title: "NƯớc ép cà rốt",
+  price: 25,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep06",
+  title: "NƯớc ép dưa hấu",
+  price: 20,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep07",
+  title: "NƯớc ép cà chua",
+  price: 25,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep08",
+  title: "NƯớc ép dưa lưới",
+  price: 25,
+  category: "NƯỚC ÉP",
+},{
+  id: "nuocep09",
+  title: "NƯớc ép cam",
+  price: 20,
+  category: "NƯỚC ÉP",
+},
+//
+{
+  id: "caphe01",
+  title: "Cacao SỮA ĐÁ",
+  price: 25,
+  category: "CAFE",
+},
+{
+  id: "caphe01",
+  title: "CÀ PHÊ SỮA ĐÁ",
+  price: 17,
+  category: "CAFE",
+},
+{
+  id: "caphe01",
+  title: "CÀ PHÊ ĐÁ",
+  price: 12,
+  category: "CAFE",
+},
+{
+  id: "caphe01",
+  title: "BẠC SĨU",
+  price: 17,
+  category: "CAFE",
+},
+
+//
+{
+  id: "SODA01",
+  title: "SODA DÂU",
+  price: 20,
+  category: "SODA",
+},{
+  id: "SODA02",
+  title: "SODA KIWI",
+  price: 20,
+  category: "SODA",
+},{
+  id: "SODA03",
+  title: "SODA BẠC HÀ",
+  price: 20,
+  category: "SODA",
+},{
+  id: "SODA04",
+  title: "SODA BIỂN XANH",
+  price: 20,
+  category: "SODA",
+},{
+  id: "SODA05",
+  title: "SODA CHANH",
+  price: 20,
+  category: "SODA",
+},{
+  id: "SODA06",
+  title: "SODA TÌNH YÊU",
+  price: 25,
+  category: "SODA",
+},
+
+{
+  id: "banhngot01",
+  title: "Bánh su",
+  price: 35,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot02",
+  title: "Mousse Chanh dây",
+  price: 30,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot03",
+  title: "Crepe",
+  price: 35,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot04",
+  title: "Pana Cotta",
+  price: 12,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot05",
+  title: "Tiramisu",
+  price: 30,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot06",
+  title: "Bông lan trứng muối",
+  price: 20,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot07",
+  title: "Bánh Phô Mai Bắp",
+  price: 35,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "banhngot09",
+  title: "Bánh Plan",
+  price: 6,
+  category: "BÁNH NGỌT",
+},
+{
+  id: "anvat01",
+  title: "Bánh tráng trộn",
+  price: 20,
+  category: "ĂN VẶT",
+},
+{
+  id: "anvat03",
+  title: "Mì Ý",
+  price: 30,
+  category: "ĂN VẶT",
+},
+{
+  id: "anvat03",
+  title: "Mỳ Cay",
+  price: 40,
+  category: "ĂN VẶT",
+},
+{
+  id: "anvat04",
+  title: "Da Heo",
+  price: 20,
+  category: "ĂN VẶT",
+},
+{
+  id: "anvat05",
+  title: "Cơm Cháy",
+  price: 20,
+  category: "ĂN VẶT",
+},
+];
+
+ // <img src=${item?.img}  width="70" class="showcase-img">
+// const x= document.getElementById("productrender")
+var html = "";
+products.map((item)=>{
+  html += `
+  <div  class="showcase">
+
+  <a href="#" class="showcase-img-box">
+   
+  </a>
+
+  <div class="showcase-content">
+
+    <a href="#">
+      <h4 class="showcase-title uppercase" style="text-transform: uppercase;">${item.title}</h4>
+    </a>
+
+    <a href="#" class="showcase-category">${item.category}</a>
+
+    <div class="price-box">
+      <p class="price">${item.price}.000 VNĐ</p>
+     
+    </div>
+
+  </div>
+
+</div>
+  `
+})
+
+document.querySelector("#xxxxxxxxxxxxxxxxxxxxxxxxxx").innerHTML= html
+
+
+const trasua= products.filter((item)=>{
+  return item.category =="TRÀ SỮA"
+})
+const suachua= products.filter((item)=>{
+  return item.category =="SỮA CHUA"
+})
+const yaourt= products.filter((item)=>{
+  return item.category =="YAOURT"
+})
+const tra= products.filter((item)=>{
+  return item.category =="TRÀ"
+})
+const sinhto= products.filter((item)=>{
+  return item.category =="SINH TỐ"
+})
+const daxay= products.filter((item)=>{
+  return item.category =="ĐÁ XAY"
+})
+const topping= products.filter((item)=>{
+  return item.category =="TOPPING"
+})
+const nuocep= products.filter((item)=>{
+  return item.category =="NƯỚC ÉP"
+})
+const caphe= products.filter((item)=>{
+  return item.category =="CAFE"
+})
+const soda= products.filter((item)=>{
+  return item.category =="SODA"
+})
+const banhngot= products.filter((item)=>{
+  return item.category =="BÁNH NGỌT"
+})
+const anvat= products.filter((item)=>{
+  return item.category =="ĂN VẶT"
+})
+const thuocla= products.filter((item)=>{
+  return item.category =="THUỐC LÁ"
+})
+
+
+var html_trasua = "";
+var html_suachua = "";
+var html_yaourt = "";
+var html_tra = "";
+var html_sinhto = "";
+var html_daxay = "";
+var html_topping = "";
+var html_nuocep = "";
+var html_caphe = "";
+var html_soda = "";
+var html_banhngot = "";
+var html_anvat = "";
+var html_thuocla = "";
+
+
+
+
+trasua.map((item)=>{
+  html_trasua += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+
+suachua.map((item)=>{
+  html_suachua += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+
+yaourt.map((item)=>{
+  html_yaourt += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+
+tra.map((item)=>{
+  html_tra += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+
+sinhto.map((item)=>{
+  html_sinhto += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+daxay.map((item)=>{
+  html_daxay += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+topping.map((item)=>{
+  html_topping += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+nuocep.map((item)=>{
+  html_nuocep += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+caphe.map((item)=>{
+  html_caphe += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+console.log(nuocep)
+soda.map((item)=>{
+  html_soda += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+banhngot.map((item)=>{
+  html_banhngot += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+
+anvat.map((item)=>{
+  html_anvat += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+thuocla.map((item)=>{
+  html_thuocla += `
+  <li  class="sidebar-submenu-category">
+  <a href="#" class="sidebar-submenu-title">
+    <p class="product-name" style=" text-transform: capitalize">${item.title}</p>
+    <data value=${item.price} class="stock" title=${item.title}>${item.price}.000 VNĐ</data>
+  </a>
+</li>
+  `
+})
+console.log(html_nuocep)
+document.querySelector("#html_trasua").innerHTML= html_trasua
+document.querySelector("#html_suachua").innerHTML= html_suachua
+document.querySelector("#html_yaourt").innerHTML= html_yaourt
+document.querySelector("#html_tra").innerHTML= html_tra
+document.querySelector("#html_sinhto").innerHTML= html_sinhto
+document.querySelector("#html_daxay").innerHTML= html_daxay
+document.querySelector("#html_topping").innerHTML= html_topping
+document.querySelector("#html_nuocep").innerHTML= html_nuocep
+
+document.querySelector("#html_cafe").innerHTML= html_caphe
+document.querySelector("#html_soda").innerHTML= html_soda
+document.querySelector("#html_banhngot").innerHTML= html_banhngot
+document.querySelector("#html_anvat").innerHTML= html_anvat
+document.querySelector("#html_thuocla").innerHTML= html_thuocla
+
+
+
+
+
+// console.log(html)
+
+
+
+
+// export default products;
--- src/pages/Contact.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-  return <div>WEBSITE ĐANG TRONG QUÁ TRÌNH PHÁT TRIỂN</div>;
-};
-
-export default Contact;
